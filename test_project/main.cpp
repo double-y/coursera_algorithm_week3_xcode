e3197a1413432967f52d2e1bf44ff3a3144c0d99
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-12-15 16:19:00 -0700

    add catch.hpp

--- test_project/main.cpp
+++ test_project/main.cpp
@@ -6,10 +6,10 @@
 //  Copyright © 2015 安田洋介. All rights reserved.
 //
 
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+TEST_CASE("exec"){
 }
